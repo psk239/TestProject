91141ed82da87214fabd69832de07ec7afdc903c
Paul Kim <thewordofgod89@gmail.com>
2014-10-27 20:26:20 -0700

    Initial commit again

---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Paul Kim on 10/27/14.
+//  Copyright (c) 2014 Paul Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by Paul Kim on 10/27/14.
+//  Copyright (c) 2014 Paul Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
